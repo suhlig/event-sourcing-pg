48d3c6c66aa756a3f8512958c6fa8d471d4bf364
Steffen Uhlig <steffen@familie-uhlig.net>
2018-02-22 23:10:12 +0100

    Fix indentation

--- db/migrations/004_users_view.rb
+++ db/migrations/004_users_view.rb
@@ -1,13 +1,13 @@
 Sequel.migration do
   up do
     run <<~EOS
-    create materialized view users_view as
-     with t as (
-       select *, row_number() over(partition by uuid order by inserted_at desc) as row_number
-       from events
-       where type = 'user_update'
-     )
-     select uuid, body->>'name' as name, inserted_at from t where row_number = 1;
+      create materialized view users_view as
+        with t as (
+          select *, row_number() over(partition by uuid order by inserted_at desc) as row_number
+          from events
+          where type = 'user_update'
+        )
+      select uuid, body->>'name' as name, inserted_at from t where row_number = 1;
     EOS
   end
 
