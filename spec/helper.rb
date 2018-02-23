3b1f2a16153784ea3055f76683244e70c5b1319f
Steffen Uhlig <steffen@familie-uhlig.net>
2018-02-24 00:16:10 +0100

    Do not truncate the database before a test
    Tests no longer rely on the global count of users

--- spec/helper.rb
+++ spec/helper.rb
@@ -41,10 +41,4 @@ RSpec.configure do |config|
   # test failures related to randomization by passing the same `--seed` value
   # as the one that triggered the failure.
   Kernel.srand config.seed
-
-  config.before(:suite) do
-    %i[events users].each do |table|
-      Sequel::Model.db[table].truncate
-    end
-  end
 end
