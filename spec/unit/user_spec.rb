1e4f904039f060886c4ae1be030bc2cf2bdd9afe
Steffen Uhlig <steffen@familie-uhlig.net>
2018-02-23 22:56:56 +0100

    Generate UUIDs

--- spec/unit/user_spec.rb
+++ spec/unit/user_spec.rb
@@ -1,39 +1,42 @@
 # frozen_string_literal: true
+require 'securerandom'
 
-describe :users do
+describe :User do
   context 'an insert event occurred' do
-    before(:all) do
+    let(:uuid) { SecureRandom.uuid }
+
+    before do
       Sequel::Model.db[:events].insert(
         type: 'user_create',
-        uuid: '11111111-1111-1111-1111-111111111111',
+        uuid: uuid,
         body: '{"name": "blah"}',
       )
     end
 
     it 'has a new user' do
-      expect(Sequel::Model.db[:users].count).to eq(1)
-      expect(Sequel::Model.db[:users].first[:name]).to eq('blah')
+      user = Sequel::Model.db[:users].where(uuid: uuid)
+      expect(user.first[:name]).to eq('blah')
     end
 
     context 'an update event occurred' do
-      before(:all) do
+      before do
         Sequel::Model.db[:events].insert(
           type: 'user_update',
-          uuid: '11111111-1111-1111-1111-111111111111',
+          uuid: uuid,
           body: '{"name": "fasel"}',
         )
       end
 
       it 'has updated attributes' do
-        expect(Sequel::Model.db[:users].count).to eq(1)
-        expect(Sequel::Model.db[:users].first[:name]).to eq('fasel')
+        user = Sequel::Model.db[:users].where(uuid: uuid)
+        expect(user.first[:name]).to eq('fasel')
       end
 
       context 'the materialized view is up to date' do
-        before(:all) do
+        before do
           Sequel::Model.db[:events].insert(
             type: 'user_update',
-            uuid: '11111111-1111-1111-1111-111111111111',
+            uuid: uuid,
             body: '{"name": "someone"}',
           )
         end
@@ -43,8 +46,8 @@ describe :users do
         end
 
         it 'has the latest user data' do
-          expect(Sequel::Model.db[:users_view].count).to eq(1)
-          expect(Sequel::Model.db[:users].first[:name]).to eq('someone')
+          user = Sequel::Model.db[:users].where(uuid: uuid)
+          expect(user.first[:name]).to eq('someone')
         end
       end
     end
